41cbb554bda8da4cdda2fd74273e073ccaa420ef
Wiley Bennett <wileybenet@users.noreply.github.com>
2015-11-03 15:31:16 -0600

    Update steel.js

--- src/steel.js
+++ src/steel.js
@@ -1,4 +1,4 @@
-window.steel = function() {
+window.steel = {
 	// Add logic to this object in any way you desire. 
 	// Test the service by opening test/index.html in any browser.
 	// Email us when all the tests pass!
